a7b80eee3a4d044a916b6de904167b56ead63684
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-09 12:41:45 +0100

    8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-            Scanner scan = new Scanner(System.in);
-            scan.useLocale(Locale.US);
-            System.out.println("Podaj liczbę z przecinkiem:");
-            float liczba =  scan.nextFloat();
-            System.out.println(liczba);
+        int tab1[] = {1, 2, 3};//deklaracja tablicy wraz z inicjalizacją
+        int[] tab2 = {1, 2, 3};//deklaracja tablicy wraz z inicjalizacją
 
+        String[] tablicaImion = new String[4];//Deklaracja 4 el. tablicy Stringów
+
+        tablicaImion[0] = "Tomek";//inicjalizacja (przypisanie) wartości
+        tablicaImion[1] = "Anna";//inicjalizacja wartości
+        tablicaImion[2] = "Łukasz";//inicjalizacja wartości
+
+        //Wypisanie wartości z tablicy z wykorzystaniem pętli foreach:
+        for(String imie : tablicaImion){
+            System.out.println(imie);
+        }
 
     }
 }
