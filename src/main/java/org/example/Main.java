78283ea8534e782eb70842a215972f0a46ded970
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-09 12:05:27 +0100

    4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        for(int i = 0; i <= 10 ; i++){
-           if(i % 2 != 0 && i != 0){
-               continue;
-           }else{
-               System.out.println(i);
-           }
-        }
-
+        //1. Start
+        Scanner scan = new Scanner(System.in);
+        //2. Podaj a (blok wejścia / wyjścia)
+        System.out.println("Podaj a:");
+        int a = scan.nextInt();
+        //3. Podaj b (blok wejścia / wyjścia)
+        System.out.println("Podaj b:");
+        int b = scan.nextInt();
+        //4. Podaj c (blok wejścia / wyjścia)
+        System.out.println("Podaj c:");
+        int c = scan.nextInt();
+        //5. Wyliczenie sumy (blok operacji)
+        int suma = a + b + c;
+        //6. Blok wyjścia wypisz suma:
+        System.out.println("Suma wartości a, b i c: " + suma);
+        //7. stop
     }
 }
