1ae80f4f7fe796f4d9988ee6ad65c66198590cad
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-09 12:07:42 +0100

    5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //1. Start
-        Scanner scan = new Scanner(System.in);
-        //2. Podaj a (blok wejścia / wyjścia)
-        System.out.println("Podaj a:");
-        int a = scan.nextInt();
-        //3. Podaj b (blok wejścia / wyjścia)
-        System.out.println("Podaj b:");
-        int b = scan.nextInt();
-        //4. Podaj c (blok wejścia / wyjścia)
-        System.out.println("Podaj c:");
-        int c = scan.nextInt();
-        //5. Wyliczenie sumy (blok operacji)
-        int suma = a + b + c;
-        //6. Blok wyjścia wypisz suma:
-        System.out.println("Suma wartości a, b i c: " + suma);
-        //7. stop
+        try {
+            //1. Start
+            Scanner scan = new Scanner(System.in);
+            //2. Podaj a (blok wejścia / wyjścia)
+            System.out.println("Podaj a:");
+            int a = scan.nextInt();
+            //3. Podaj b (blok wejścia / wyjścia)
+            System.out.println("Podaj b:");
+            int b = scan.nextInt();
+            //4. Podaj c (blok wejścia / wyjścia)
+            System.out.println("Podaj c:");
+            int c = scan.nextInt();
+            //5. Wyliczenie sumy (blok operacji)
+            int suma = a + b + c;
+            //6. Blok wyjścia wypisz suma:
+            System.out.println("Suma wartości a, b i c: " + suma);
+            //7. stop
+        }catch (Exception ex){
+            System.out.println("Wyjątek: " + ex);
+        }
     }
 }
