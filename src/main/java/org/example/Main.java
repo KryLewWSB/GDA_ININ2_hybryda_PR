c3321f352a7a16f478338eb484cfa863c20fb2ab
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-10 18:43:08 +0100

    17

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,10 @@ public class Main {
         //Scanner scan = new Scanner(System.in);
         int liczba = 5;
 
-        Pracownik pracownik1 = new Pracownik();
-        pracownik1.imie = "Adam";
-        pracownik1.nazwisko = "Kowalski";
+        Pracownik pracownik1 = new Pracownik("Adam", "Kowalski", Dzial.KSIEGOWOSC, 5_000);
+        Pracownik pracownik2 = new Pracownik("Anna",  "Nowak", Dzial.IT, 8_000);
 
-        Pracownik pracownik2 = new Pracownik();
-        pracownik2.imie = "Anna";
-        pracownik2.nazwisko = "Nowak";
-        System.out.println("Imię pierwszego pracownika: " + pracownik1.imie +
-                            "\nNazwisko pierwszego pracownika:" + pracownik1.nazwisko);
-        System.out.println("Imię drugiego pracownika: " + pracownik2.imie +
-                            "\nNazwisko drugiego pracownika:" + pracownik2.nazwisko);
+        pracownik1.wypiszDane();
+        pracownik2.wypiszDane();
     }
 }
--- src/main/java/org/example/Pracownik.java
+++ src/main/java/org/example/Pracownik.java
@@ -1,6 +1,25 @@
 package org.example;
 
-public class Pracownik {
-    public String imie;
-    public String nazwisko;
+public class Pracownik implements InformacjeOPracowniku {
+    private String imie;
+    private String nazwisko;
+    private Dzial dzial;
+    private int wynagrodzenie;
+
+    public Pracownik(String imie, String nazwisko, Dzial dzial, int wynagrodzenie) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.dzial = dzial;
+        this.wynagrodzenie = wynagrodzenie;
+    }
+
+    @Override
+    public void wypiszDane() {
+        System.out.println("\n------------------------" +
+                            "\nImię pracownika: " + this.imie +
+                            "\nNazwisko pracownika: " + this.nazwisko +
+                            "\nDział pracownika: " + this.dzial +
+                            "\nWynagrodzenie pracownika: " + this.wynagrodzenie +
+                            "\n------------------------");
+    }
 }
